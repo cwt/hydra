8db382ba897880ffd3bb2ddb1e8c8eb80efe7459
Chaiwat Suttipongsakul <cwt@bashell.com>
2020-11-30 10:41:44 +0700

    remove local project specific script

--- sysinfo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./hydra.py hosts.all 'source /etc/os-release ; echo CPU: $(nproc) x $(lscpu |grep "^Model name" |tr -s " " |cut -d ":" -f2) Mem: $(free -h |grep ^Mem |tr -s " " |cut -d " " -f2) OS: $PRETTY_NAME'
